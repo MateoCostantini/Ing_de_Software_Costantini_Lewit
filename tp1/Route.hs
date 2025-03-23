7f89959633769a3524104a35c0c11d6bff98f91c
Mateo Costantini <costantinim@udesa.edu.ar>
2025-03-23 12:00:39 -0300

    ahora si

--- tp1/Route.hs
+++ tp1/Route.hs
@@ -15,26 +15,23 @@ newR cities = Rou cities
 --                                             | otherwise = inOrderR (Rou cs) cityQuery citySearch 
 
 inOrderR :: Route -> String -> String -> Bool  
-inOrderR (Rou cities) cityQuery citySearch
-    | null cities               = error "La ruta está vacía."
-    | cityQuery == citySearch   = True
-    | not (elem cityQuery cities) = False
-    | not (elem citySearch cities) = True
-    | otherwise = firstOccurrence cities cityQuery < lastOccurrence cities citySearch
-  
+inOrderR (Rou cities) cityQuery citySearch  | null cities               = error "La ruta está vacía."
+                                            | cityQuery == citySearch   = True
+                                            | not (elem cityQuery cities) = False
+                                            | not (elem citySearch cities) = True
+                                            | otherwise = firstOccurrence cities cityQuery < lastOccurrence cities citySearch
+                                          
 firstOccurrence :: [String] -> String -> Int
 firstOccurrence [] _ = error "La ciudad no está en la ruta."
-firstOccurrence (c:cs) city
-      | c == city = 0
-      | otherwise = 1 + firstOccurrence cs city
+firstOccurrence (c:cs) city | c == city = 0
+                            | otherwise = 1 + firstOccurrence cs city
 
 lastOccurrence :: [String] -> String -> Int
 lastOccurrence cities city = lastOccurrenceAux cities city 0 (-1)
   where
       lastOccurrenceAux [] _ _ current = current
-      lastOccurrenceAux (c:cs) city idx current
-          | c == city = lastOccurrenceAux cs city (idx+1) idx
-          | otherwise = lastOccurrenceAux cs city (idx+1) current
+      lastOccurrenceAux (c:cs) city idx current | c == city = lastOccurrenceAux cs city (idx+1) idx
+                                                | otherwise = lastOccurrenceAux cs city (idx+1) current
 
 
 inRouteR :: Route -> String -> Bool -- indica si la ciudad consultada está en la ruta

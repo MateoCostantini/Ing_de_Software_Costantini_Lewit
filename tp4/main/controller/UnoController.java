aa63b836fa0461e25e36267e7cde782c0d5c232b
MateoCostantini <101668925+MateoCostantini@users.noreply.github.com>
2025-06-15 01:02:13 -0300

    Add files via upload

--- tp4/main/controller/UnoController.java
+++ tp4/main/controller/UnoController.java
@@ -1,7 +1,6 @@
 package org.udesa.unoback.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -32,14 +31,12 @@ public class UnoController {
     public ResponseEntity newMatch(@RequestParam List<String> players){
         return ResponseEntity.ok(unoService.newMatch(players));
     }
-    // curl -X POST "http://localhost:8080/newmatch?players=Lolo&players=Pepe" -H "Accept: application/json"
 
     @PostMapping("play/{matchId}/{player}")
     public ResponseEntity play(@PathVariable UUID matchId, @PathVariable String player, @RequestBody JsonCard card) {
         unoService.play(matchId, player, card.asCard());
         return ResponseEntity.ok().build();
     }
-    // curl -X POST "http://localhost:8080/play/35655cdd-83cf-4b72-a02e-a4b713c03291/Lolo" -H "Content-Type: Application/json" -d '{"color":"Blue","number":8,"type":"NumberCard","shout":false}'
 
 
     @PostMapping("draw/{matchId}/{player}")
@@ -47,19 +44,16 @@ public class UnoController {
         unoService.draw(matchId, player);
         return ResponseEntity.ok().build();
     }
-    // POST draw/{matchId}/{player}
 
     @GetMapping("activecard/{matchId}")
     public ResponseEntity activeCard( @PathVariable UUID matchId ) {
         return ResponseEntity.ok(unoService.activeCard(matchId).asJson());
     }
-    // curl -X GET "http://localhost:8080/activecard/35655cdd-83cf-4b72-a02e-a4b713c03291" -H "Accept: application/json"
 
     @GetMapping("playerhand/{matchId}")
     public ResponseEntity playerHand( @PathVariable UUID matchId ) {
         return ResponseEntity.ok(unoService.playerHand(matchId).stream().map(each -> each.asJson()));
     }
-    // curl -X GET "http://localhost:8080/playerhand/35655cdd-83cf-4b72-a02e-a4b713c03291" -H "Accept: application/json"
 
 
 
